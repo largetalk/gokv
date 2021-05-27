2473c765d402175e87a0f3619006935f0009ba22
Arthur <arthur_wang@adsame.com>
2021-05-27 16:49:54 +0800

    tcp server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module adsame.com/m
+
+go 1.16
--- src/main.go
+++ src/main.go
@@ -1,8 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func handleConnection(c net.Conn) {
+	fmt.Printf("Serving %sn", c.RemoteAddr().String())
+	for {
+		netData, err := bufio.NewReader(c).ReadString('n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		temp := strings.TrimSpace(netData)
+		if temp == "STOP" {
+			break
+		}
+
+		result := strconv.Itoa(rand.Intn(10)) + "n"
+		c.Write([]byte(string(result)))
+	}
+}
 
 func main() {
-	fmt.Println("hello")
-	fmt.Println("world")
+	arguments := os.Args
+	if len(arguments) == 1 {
+			fmt.Println("Please provide a port number!")
+			return
+	}
+
+	PORT := ":" + arguments[1]
+	l, err := net.Listen("tcp4", PORT)
+	if err != nil {
+			fmt.Println(err)
+			return
+	}
+	defer l.Close()
+	rand.Seed(time.Now().Unix())
+
+	for {
+			c, err := l.Accept()
+			if err != nil {
+					fmt.Println(err)
+					return
+			}
+			go handleConnection(c)
+	}
 }
